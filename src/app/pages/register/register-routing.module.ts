2b1f50b7a250a1306022775c12123c7cbc7ab334
ruthadorable <ruthadorable@live.com>
2024-05-30 16:59:01 +0200

    registerpage

--- src/app/pages/register/register-routing.module.ts
+++ src/app/pages/register/register-routing.module.ts
@@ -1,17 +1,3 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { RegisterPage } from './register.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: RegisterPage
-  }
-];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class RegisterPageRoutingModule {}
+version https://git-lfs.github.com/spec/v1
+oid sha256:01be56419a3083c9e6437cc11e5417634433dfc92daef509eb0af0ab57fcc044
+size 353
